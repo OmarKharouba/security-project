e59080e45c018ed2904174284a06c41d76419a80
kharouba <omarkharouba4@gmail.com>
2019-05-01 02:46:31 +0200

    added authorization checks

--- routes/message.js
+++ routes/message.js
@@ -24,7 +24,14 @@ router.get('/', passport.authenticate("jwt", { session: false }), (req, res, nex
 router.get('/:id', passport.authenticate("jwt", { session: false }), (req, res, next) => {
   let response = { success: true };
   Conversation.getConversationById(req.params.id, (err, conversation) => {
-    if (err) {
+
+    let participants = conversation.participants || conversation._doc.participants;
+    let found = false;
+    participants.forEach(p => {
+      if (p.username == req.user.username)
+        found = true;
+    })
+    if (err || !found) {
       response.success = false;
       response.msg = "There was an error on getting the conversation";
       res.json(response);
@@ -39,8 +46,11 @@ router.get('/:id', passport.authenticate("jwt", { session: false }), (req, res, 
 // get conversation
 router.get('/:name1/:name2', passport.authenticate("jwt", { session: false }), (req, res, next) => {
   let response = { success: true };
+
+  let found = (req.user.username == req.params.name1) || (req.user.username == req.params.name2);
+
   Conversation.getConversationByName(req.params.name1, req.params.name2, (err, conversation) => {
-    if (err) {
+    if (err || !found) {
       response.success = false;
       response.msg = "There was an error on getting the conversation";
       res.json(response);
